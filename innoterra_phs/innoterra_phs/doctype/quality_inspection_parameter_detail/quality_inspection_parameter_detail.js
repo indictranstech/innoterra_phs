16ed2d47159cf19024fd61742d63c60ab49265dd
Nilima Dhokane <nilima.d@indictranstech.com>
2022-06-07 11:44:49 +0530

    [quality inspection parameter detail]

--- innoterra_phs/innoterra_phs/doctype/quality_inspection_parameter_detail/quality_inspection_parameter_detail.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, Indictranstech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Quality Inspection Parameter Detail', {
-	// refresh: function(frm) {
-
-	// }
-});
--- innoterra_phs/innoterra_phs/doctype/quality_inspection_parameter_detail/test_quality_inspection_parameter_detail.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Indictranstech and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestQualityInspectionParameterDetail(unittest.TestCase):
-	pass
