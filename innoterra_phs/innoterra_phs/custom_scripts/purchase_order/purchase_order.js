67545a851a700fae0f94a48b6b808d3fd1ffa5b6
Ashishkumar <ashishkumar.r@indictrans.in>
2022-10-19 16:44:53 +0530

    Able to create SO from PO with custom_button

--- innoterra_phs/innoterra_phs/custom_scripts/purchase_order/purchase_order.js
+++ innoterra_phs/innoterra_phs/custom_scripts/purchase_order/purchase_order.js
@@ -5,7 +5,13 @@ frappe.ui.form.on('Purchase Order', {
         frappe.model.open_mapped_doc({
             method: "innoterra_phs.innoterra_phs.custom_scripts.purchase_order.purchase_order.make_Sales_order",
             frm: cur_frm
-       }, ('Create')); 
+       }, ('Create'));
     })
-  }
+  },
+  validate(frm){
+    if (frappe.datetime.now_date() > cur_frm.doc.transaction_date){
+        frappe.validate = false
+        frappe.throw(`Date should not be less then  ${frappe.datetime.now_date()}`)
+    }
+  },
 });
