b697615f39bccbc838ef29600917081a679b2d28
saadchaudharry <saadchaudhary646@gmail.com>
2022-10-20 09:52:45 +0530

    [supplier fix]

--- innoterra_phs/innoterra_phs/custom_scripts/supplier/supplier.js
+++ innoterra_phs/innoterra_phs/custom_scripts/supplier/supplier.js
@@ -1,10 +1,13 @@
-frappe.ui.form.on("Supplier", {
-	before_save: function (frm) {
-        
-		if (frm.doc.is_farmer == 1) {
-			frm.set_value("supplier_type", 'Individual');
-            refresh_field("supplier_type")
-            cur_frm.save();
-		}
-      }
-    })
+frappe.ui.form.on('Supplier', {
+	validate(frm) {
+		// your code here
+        if ( frm.doc.is_supplier == 1 ){
+            frm.set_value('supplier_type','Individual')
+            frm.refresh_field('supplier_type')
+        }
+	},
+    is_supplier(frm){
+        frm.set_value('supplier_type','Individual')
+        frm.refresh_field('supplier_type')
+    }
+})
