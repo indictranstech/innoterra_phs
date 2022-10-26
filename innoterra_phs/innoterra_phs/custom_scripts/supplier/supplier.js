b7d502d9dab0c8088ff4d89ae590893c330b9f44
pradyotraina <100671540+pradyotraina@users.noreply.github.com>
2022-10-26 12:01:20 +0530

    set supplier type individual for farmers

--- innoterra_phs/innoterra_phs/custom_scripts/supplier/supplier.js
+++ innoterra_phs/innoterra_phs/custom_scripts/supplier/supplier.js
@@ -1,13 +1,19 @@
 frappe.ui.form.on('Supplier', {
 	validate(frm) {
 		// your code here
-        if ( frm.doc.is_supplier == 1 ){
+        if ( frm.doc.is_supplier == 1 || frm.doc.is_farmer == 1){
             frm.set_value('supplier_type','Individual')
             frm.refresh_field('supplier_type')
         }
 	},
-    is_supplier(frm){
-        frm.set_value('supplier_type','Individual')
-        frm.refresh_field('supplier_type')
-    }
+	is_supplier(frm){
+	frm.set_value('supplier_type','Individual')
+	frm.refresh_field('supplier_type')
+	},
+	is_farmer(frm){
+		if(frm.doc.is_farmer)    {
+			frm.set_value("supplier_type", "Individual");
+			frm.refresh_field('supplier_type');
+		    } 
+    	}
 })
