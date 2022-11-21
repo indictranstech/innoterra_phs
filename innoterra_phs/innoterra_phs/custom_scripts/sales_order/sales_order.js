9c5b1a337ef002323ccbb3889d10490f7b7c5fa1
sameer-itt <sameer.s@indictranstech.com>
2022-11-21 13:05:37 +0530

    SO code comment and custom fields fixtures

--- innoterra_phs/innoterra_phs/custom_scripts/sales_order/sales_order.js
+++ innoterra_phs/innoterra_phs/custom_scripts/sales_order/sales_order.js
@@ -1,31 +1,32 @@
 frappe.ui.form.on('Sales Order', {
-    
-    setup: function (frm) {
-       if( frm.doc.is_internal_customer == 0 && (frm.doc.po_number !== undefined || frm.doc.po_number !== "" ) ){
+    // commented this setup trigger because field of SO were unset due to below code even after submit
+    // Doing this after communicating(on 21/11/2022) the same with Nilima Dhokane and Snehal Sathe
+    // setup: function (frm) {
+    //    if( frm.doc.is_internal_customer == 0 && (frm.doc.po_number !== undefined || frm.doc.po_number !== "" ) ){
            
-       frm.set_value('inter_company_order_reference',"")
-       frm.set_value('po_number',"")
-       frm.set_value('customer',"")
-       frm.set_value('represents_company',"")
-       frm.set_value('is_internal_customer',0)
+    //    frm.set_value('inter_company_order_reference',"")
+    //    frm.set_value('po_number',"")
+    //    frm.set_value('customer',"")
+    //    frm.set_value('represents_company',"")
+    //    frm.set_value('is_internal_customer',0)
  
-        frm.refresh_field('inter_company_order_reference');
-       frm.refresh_field('po_number');
-       frm.refresh_field('customer');
-       frm.refresh_field('represents_company');
-       frm.refresh_field('is_internal_customer');
+    //     frm.refresh_field('inter_company_order_reference');
+    //    frm.refresh_field('po_number');
+    //    frm.refresh_field('customer');
+    //    frm.refresh_field('represents_company');
+    //    frm.refresh_field('is_internal_customer');
  
  
-       $.each(frm.doc.items || [], function (i, v) {
-           console.log(v)
-         frappe.model.set_value(v.doctype, v.name, "delivery_by_supplier",0)
-         frappe.model.set_value(v.doctype, v.name, "prevdoc_docname","")
+    //    $.each(frm.doc.items || [], function (i, v) {
+    //        console.log(v)
+    //      frappe.model.set_value(v.doctype, v.name, "delivery_by_supplier",0)
+    //      frappe.model.set_value(v.doctype, v.name, "prevdoc_docname","")
  
-       })
-       frm.refresh_field('items');
+    //    })
+    //    frm.refresh_field('items');
  
-    }
-    },
+    // }
+    // },
     is_vla(frm) {
         frm.set_query('vla', () => {
             return {
