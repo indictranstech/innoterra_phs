8c5f554685c6b7eeb7fd3ba6d952e568c863df07
sameer-itt <sameer.s@indictranstech.com>
2022-12-21 17:58:00 +0530

    Fix: Commented unused code

--- innoterra_phs/innoterra_phs/custom_scripts/address/address.js
+++ innoterra_phs/innoterra_phs/custom_scripts/address/address.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on('Address', {
 	refresh(frm) {
-		frm.set_value('territory',"");
-		frm.refresh_field("territory");
+		// frm.set_value('territory',"");
+		// frm.refresh_field("territory");
 	  if(frappe.user.has_role("PHS")===false){
 	      frm.set_df_property("city", "reqd", 1);
 	  }
