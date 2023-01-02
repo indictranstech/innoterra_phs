85a66d85c23a649aaeac4249a0cc7c3acbee385e
pradyotraina <100671540+pradyotraina@users.noreply.github.com>
2023-01-02 16:41:53 +0530

    update: farmer validation fix

--- innoterra_phs/innoterra_phs/custom_scripts/customer/customer.py
+++ innoterra_phs/innoterra_phs/custom_scripts/customer/customer.py
@@ -4,5 +4,5 @@ def validate(doc, method=None):
 	
 	if doc.mobile_no and frappe.db.exists("Customer", {'name':["!=", doc.name], 'mobile_no':doc.mobile_no, 'customer_name':doc.customer_name}):
 		frappe.throw("Duplicate Values for Customer name and Mobile No")
-	if doc.mobile_no and doc.customer_group=="Farmer" and frappe.db.exists("Customer", {'mobile_no':doc.mobile_no, 'customer_group':"Farmer"}):
+	if doc.mobile_no and doc.customer_group=="Farmer" and frappe.db.exists("Customer", {'name':["!=", doc.name],'mobile_no':doc.mobile_no, 'customer_group':"Farmer"}):
 		frappe.throw("Mobile Number exists.")
